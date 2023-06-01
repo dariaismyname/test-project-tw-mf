d6f326082fd4712238b8f73eb33a1b15411877c6
Daria <daria.cherkasova.work@gmail.com>
2023-06-01 13:07:20 +0300

    finish project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: ["./src/**/*.{html,js}"],
-  theme: {},
-  plugins: [],
+  theme: {
+    extend: {
+      keyframes: {
+        wavey: {
+          "0%, 100%": {
+            transform: "scaleY(0.5)",
+          },
+          "50%": {
+            transform: "scaleY(1.5)",
+          },
+        },
+      },
+      animation: {
+        wavey: "wavey 1000ms linear infinite",
+      },
+    },
+  },
+  plugins: [
+    require("./plugin/openVariant"),
+    require("./plugin/animationDelay"),
+    require("./plugin/tableCaption"),
+    require("./plugin/buttonPlugin"),
+    require("./plugin/outlineSolid"),
+  ],
 };
